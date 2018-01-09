72dba341711813c120cc4d282d8b705d93b40b28
suwigyarathore <Suwi.Rathore@klm.com>
2018-01-09 23:05:20 +0100

    Create private key &  public address

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const request = require("request");
+const bodyParser = require("body-parser");
+const bitCore = require("bitcore-lib");
 
 const app = express();
-let btcPrice;
-let btcBlocks;
-
-request({
-  url: "https://blockchain.info/stats?format=json",
-  json: true
-}, (error, res, body) => {
-  btcPrice = body.market_price_usd;
-  btcBlocks = body.n_blocks_total;
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Coin to the moon ' + btcPrice);
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/block', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.post('/wallet', (req, res) => {
+  let brainsrc = req.body.brainsrc;
+  console.log('brainsrc', brainsrc);
+  const input = new Buffer(brainsrc);
+  const hash = bitCore.crypto.Hash.sha256(input);
+  const bn = bitCore.crypto.BN.fromBuffer(hash);
+  const pk = new bitCore.PrivateKey(bn).toWIF();
+  const addy = new bitCore.PrivateKey(bn).toAddress();
+  res.send('The brain wallet of: ' + brainsrc + "<br>Addy:"
+    + addy + "<br>Private key:" + pk);
 });
 
 app.listen(8080, () => {
