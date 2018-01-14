d2a07068ca3202b929babbf176d5ac6e13f322e4
suwigyarathore <Suwi.Rathore@klm.com>
2018-01-14 02:07:17 +0100

    Creating wallet

--- app.js
+++ app.js
@@ -2,17 +2,39 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const bitCore = require("bitcore-lib");
+let lastPrice;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  getPrice((lastPrice) => {
+    res.render("index", {
+      lastPrice
+    });
+  })
+});
+
+app.get('/brain', (req, res) => {
+  getPrice((lastPrice) => {
+    res.render("brain", {
+      lastPrice
+    });
+  })
+});
+
+app.get('/converter', (req, res) => {
+  getPrice((lastPrice) => {
+    res.render("converter", {
+      lastPrice
+    });
+  })
 });
 
 function brainWallet (uinput, callback) {
-  const input = new Buffer(brainsrc);
+  const input = new Buffer(uinput);
   const hash = bitCore.crypto.Hash.sha256(input);
   const bn = bitCore.crypto.BN.fromBuffer(hash);
   const pk = new bitCore.PrivateKey(bn).toWIF();
@@ -20,9 +42,24 @@ function brainWallet (uinput, callback) {
   callback(pk, addy);
 }
 
+function getPrice (returnPrice) {
+  request({
+    url: "https://blockchain.info/ticker",
+    json: true
+  }, (err, res, body) => {
+    returnPrice(body.USD.buy);
+  });
+}
+
+request({
+  url: "https://blockchain.info/ticker",
+  json: true
+}, (err, res, body) => {
+  lastPrice = body.USD.buy;
+});
+
 app.post('/wallet', (req, res) => {
   let brainsrc = req.body.brainsrc;
-  console.log('brainsrc', brainsrc);
   brainWallet(brainsrc, (priv, addr) => res.send('The brain wallet of: ' + brainsrc + "<br>Addy:"
     + addr + "<br>Private key:" + priv));
 });
